275dac114c34704601fc3af1688aa418448a41ba
Thor Kuivanen <lightninthorman@Thors-MBP.lan>
2024-09-13 11:07:08 -0400

    unsure what i have done

--- src/app/audiences/page.jsx
+++ src/app/audiences/page.jsx
@@ -1,17 +1,20 @@
-// import { CampaignsTable } from "./components/CampaignsTable";
 
-async function getCampaigns() {
-	const res = await fetch("https://t-propensity-dashboard.addapptation.com/account_lists_data?api_key=6d5b9cb6-d85e-43c8-a892-b9c18dd77bac&campaigns=true");
+
+async function getAccountLists() {
+	const res = await fetch("https://t-propensity-react.addapptation.com/audiences?api_key=6d5b9cb6-d85e-43c8-a892-b9c18dd77bac");
 	const data = await res.json();
+	console.log(data)
 	return data;
 }
-
+console.log("SEE THIS?")
 export default async function Audiences() {
-	// const campaigns = await getCampaigns();
+	console.log("get account list")
+	const accounts = await getAccountLists();
+	console.log("ACCOUNTS: ",accounts)
 	return (
 		<div className='bg-white p-4 sm:p-6 lg:p-8 m-4'>
-			<h1>MADE IT</h1>
-			{/* <CampaignsTable campaigns={campaigns} /> */}
+			<h1>MADE IT!</h1>
+			{JSON.stringify(accounts, null, 2)}
 		</div>
 	);
 }
